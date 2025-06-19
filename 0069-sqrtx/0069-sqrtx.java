d06854d351002fb76fb146cf0bd788ce1db6b922
Manish Lohare <158021993+manish-lohare@users.noreply.github.com>
2025-06-19 14:54:10 +0530

    Time: 1 ms (86.79%), Space: 40.8 MB (69.69%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,5 +1,23 @@
 class Solution {
     public int mySqrt(int x) {
-        return (int)Math.sqrt(x);
+        int low=1;
+        int high=x;
+        
+        int mid=-1;
+        while(low<=high){
+            mid=low+(high-low)/2;
+            if(mid*mid==x){
+                return mid;
+            }
+            else if((long)mid*mid <= (long)x){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return Math.round(high);
+        
     }
+    
 }
